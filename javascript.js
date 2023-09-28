1e8d203d2852b4a2d1c0f189c7c76a8d365a4c0c
willRathier <willi02rm@gmail.com>
2023-09-28 13:43:40 -0400

    javascript chnages

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ let chaptersObj = {
     //chatGpt
     text: "Dans la ville de Northville, Liam, un jeune passionné de hockey, passait chaque instant libre sur la patinoire. Son rêve était simple : devenir une légende du hockey sur glace.",
     img: "assets/images/joueur_intro.jpg",
-    option: [
+    boutons: [
       { titre: "boire un smoothie", destination: "goToChapter('biere')" },
       { titre: "boire un verre d'eau", destination: "goToChapter('camp')" },
       { titre: "boire une bière", destination: "goToChapter('camp')" },
@@ -13,14 +13,14 @@ let chaptersObj = {
   biere: {
     subtitle: "Tu es pas très smat mon gars",
     text: "Tu te fais arrêter par la police pour conduite en état d'ébriété.",
-    option: [{ titre: "next", destination: "goToChapter('out')" }],
+    boutons: [{ titre: "next", destination: "goToChapter('out')" }],
   },
   camp: {
     subtitle: "Arriver au camp d'entrainement",
     //chatGpt
     text: "À son arrivée au camp d'entraînement de hockey, Alex a été accueilli par une équipe de légendes du sport, prête à lui enseigner les secrets de la glace pour devenir un futur champion.",
     img: "assets/images/arriver_au_camp.jpg",
-    option: [
+    boutons: [
       { titre: "oublier son baton", destination: "goToChapter('theKey')" },
       { titre: "arivver en retard", destination: "goToChapter('out')" },
       { titre: "faire un warm up", destination: "goToChapter('surLaGlace')" },
@@ -30,7 +30,7 @@ let chaptersObj = {
     subtitle: "tu es vraiment chanceux",
     text: "tu es tombé sur le directeur général a ta sorti de la chambre, vous avez beaucoup parlé et il a confiance en toi",
     img: "assets/images/confiance.jpg",
-    option: [
+    boutons: [
       { titre: "recommencer", destination: "goToChapter('firstChapter')" },
     ],
   },
@@ -39,7 +39,7 @@ let chaptersObj = {
     //chatGpt
     text: "Nerveux et excité, Lucas a enfilé son maillot professionnel pour sa toute première pratique avec l'équipe de hockey de la ligue majeure, réalisant enfin son rêve d'enfance sur la glace éclatante de l'aréna.",
     img: "assets/images/training.jpg",
-    option: [
+    boutons: [
       { titre: "tir partout", destination: "goToChapter('tirPartout')" },
       { titre: "les partisans", destination: "goToChapter('partisans')" },
       { titre: "etire toi", destination: "goToChapter('teamMeeting')" },
@@ -49,42 +49,72 @@ let chaptersObj = {
     subtitle: "Bien choisi",
     text: "Les étirements aident à faire un bon entrainement. Tu es maintenant à la rencontre d'équipe.",
     img: "assets/images/meeting.jpg",
-    option: [
+    boutons: [
       { titre: "manger le diner", destination: "goToChapter('diner')" },
-      { titre: "vas directement", destination: "goToChapter('')" },
-      { titre: "Faire une sieste", destination: "goToChapter('')" },
+      { titre: "vas directement", destination: "goToChapter('rencontreDG')" },
+      { titre: "Faire une sieste", destination: "goToChapter('sieste')" },
     ],
+  },
+  rencontreDG: {
+    subtitle: "La rencontre",
+    text: "Le directeur gérnéral veut te voir et il a quelque chose d'important à te dire.",
+    img: "assets/images/dg.jpg",
+    boutons: [
+      { titre: "oui", destination: "goToChapter('fin')" },
+      { titre: "non", destination: "goToChapter('grosNon')" },
+    ],
+  },
+  fin: {
+    subtitle: "Tu y es arrivé",
+    text: "Tu fais maintenant parti de l'équipe tu peux enfin signer ton premier contrat professionnel.",
+    img: "assets/images/bedard.jpg",
+    boutons: [
+      { titre: "recommencer", destination: "goToChapter('firstChapter')" },
+    ],
+  },
+  grosNon: {
+    subtitle: "Tu n'es pas le bienvenue ici",
+    text: "le directeur général de l'equipe ne veut pas te voir dans l'entourage de l'équipe tu n'as pas ta place.",
+    img: "assets/images/dg-colere.jpg",
+    boutons: [{ titre: "next", destination: "goToChapter('out')" }],
+  },
+  sieste: {
+    subtitle: "tu fais des dodos ?",
+    text: "C'est le temps de jouer au hockey pas dormir.",
+    img: "assets/images/dormir.jpg",
+    boutons: [{ titre: "next", destination: "goToChapter('out')" }],
   },
   diner: {
     subtitle: "Non, mais serieux",
     text: "Ton diner peut attendre mon homme",
-    img: "assets/img/colere.jpg",
-    option: [{ titre: "next", destination: "goToChapter('out')" }],
+    img: "assets/images/colere.jpg",
+    boutons: [{ titre: "next", destination: "goToChapter('out')" }],
   },
   tirPartout: {
     subtitle: "Bing Bang des pucks volent partout",
     text: "Tu tir partout comme un vrai tout croche.",
-    img: "assets/img/rondelles.jpg",
-    option: [{ titre: "next", destination: "goToChapter('out')" }],
+    img: "assets/images/rondelles.jpg",
+    boutons: [{ titre: "next", destination: "goToChapter('out')" }],
   },
   partisans: {
     subtitle: "Manque de jugement de ta part",
     text: "Non mais mon homme tu fais quoi tu vas blesser quelqu'un.",
-    img: "assets/img/colere.jpg",
-    option: [{ titre: "next", destination: "goToChapter('out')" }],
+    img: "assets/images/colere.jpg",
+    boutons: [{ titre: "next", destination: "goToChapter('out')" }],
   },
   out: {
     subtitle: "tu es out le gros",
     //chatGpt
     text: "Après une saison décevante, Jake a été retranché de son équipe de hockey bien-aimée, laissant derrière lui des souvenirs indélébiles et une détermination à revenir plus fort.",
     img: "assets/images/decu.jpg",
-    option: [
+    boutons: [
       { titre: "recommencer", destination: "goToChapter('firstChapter')" },
     ],
   },
 };
 
-function goToChapter(chaptersObj) {
-  console.log(chaptersObj[chapterName]["subtitle"]);
-  console.log(chaptersObj[chapterName]["text"]);
+function goToChapter(chapterName) {
+  console.log(chaptersObj[chapterName].subtitle);
+  console.log(chaptersObj[chapterName].text);
+  console.log(chaptersObj[chapterName].boutons);
 }
